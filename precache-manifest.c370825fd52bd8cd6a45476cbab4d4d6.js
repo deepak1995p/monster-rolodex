9f48ddbd10d03db926fba1121bb7c8be08b7b4bc
deepak1995p <deepak1995p@users.github.com>
2020-07-26 00:58:05 +0530

    Updates

---
+++ precache-manifest.c370825fd52bd8cd6a45476cbab4d4d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5055f8cf1754afb4b2a459fbccbace5e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a4f2542f23e4bc8ef546",
+    "url": "/monster-rolodex/static/css/main.c9031c51.chunk.css"
+  },
+  {
+    "revision": "299f65059b0504546074",
+    "url": "/monster-rolodex/static/js/2.eca8dab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.eca8dab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f2542f23e4bc8ef546",
+    "url": "/monster-rolodex/static/js/main.9eac6ea1.chunk.js"
+  },
+  {
+    "revision": "41a66e52edf13a21070c",
+    "url": "/monster-rolodex/static/js/runtime-main.13206672.js"
+  }
+]);
